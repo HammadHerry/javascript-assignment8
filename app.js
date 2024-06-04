cbcd937502b82a13aa5dce75acf61a8d8c98fb09
HammadHerry <hammadherry81@gmail.com>
2024-06-04 06:55:15 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var num = "2" + "2";
+console.log(num);
+
+var m ='dolly'
+alert("hello "+m+"")
+
+ alert("33" + 3);
+
+var p = "zindabad"
+alert("pakistan"+p+"");
+
+
+ var herry = "Hello, " + 42;
+
+ var string1 = "hello,";
+ var string2 = "javascript!";
+ var bullet = string1 + string2;
+ console.log(bullet);
+
+
